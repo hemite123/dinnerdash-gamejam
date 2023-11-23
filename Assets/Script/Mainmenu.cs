188008c54eb60969d3f81a10e4e1645cafc6b625
MiteZ <hemitz90@gmail.com>
2023-11-24 01:03:15 +0700

    add music and main menu testing

--- Assets/Script/Mainmenu.cs
+++ Assets/Script/Mainmenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Mainmenu : MonoBehaviour
 {
+    public GameObject background;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
